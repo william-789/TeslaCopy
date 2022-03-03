dcd9fe983cac3b9079e8494c4735aae36b602943
william-789 <riberackw@gmail.com>
2022-03-03 17:17:26 +0000

    Navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const NavbarBox = styled.nav `
+width: 100%;
+display: flex;
+align-items: center;
+justify-content: space-between;
+
+`;
 
 function Navbar() {
   return (
-    <>
-        Navbar
-    </>
+    <NavbarBox>
+        <IconTesla />
+        <Products />
+        <MenuGeneral />
+    </NavbarBox>
   )
 }
+/*IconTesla - icon
+Products: Model S, Model 3, Model X, Solar Roof, Solar Panels
+MenuGeneral: Shop Account Menu
+tesla.com
+*/
 
 export default Navbar
